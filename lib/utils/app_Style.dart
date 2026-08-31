797f8f1e7e0df67f5a819d6109a340f6af564b35
MohamedShawky <shawkymohamed138@gmail.com>
2026-08-31 19:23:28 +0300

    initial flutter project

---
+++ lib/utils/app_Style.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:greduation_movies_fluter/utils/app_color.dart';
+
+class AppStyle{
+  static TextStyle bold24White=GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor
+
+  );
+
+  static TextStyle bold36White=GoogleFonts.roboto(
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor
+
+  );
+
+
+    static TextStyle bold20Date=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.dateColor
+
+  );
+  
+    static TextStyle regular16White=GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor
+
+  );
+    static TextStyle regular16Yellow=GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yellowColor
+
+  );
+
+    static TextStyle regular20White=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor
+
+  );
+
+    static TextStyle regular14Yollew=GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yellowColor
+
+  );
+    static TextStyle bold20White=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor
+
+  );
+
+    static TextStyle regular20Black=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.blackColor
+
+  );
+
+    static TextStyle black14White=GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w900,
+    color: AppColors.whiteColor
+
+  );
+    static TextStyle black14Yellow=GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w900,
+    color: AppColors.yellowColor
+
+  );
+
+    static TextStyle regular15Yellow=GoogleFonts.roboto(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yellowColor
+
+  );
+    static TextStyle regular16Black=GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.blackColor
+
+  );
+     static TextStyle semiBold20Black=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.blackColor
+
+  );
+
+    static TextStyle bold20Black=GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackColor
+
+  );
+
+
+
+
+
+
+
+
+}
