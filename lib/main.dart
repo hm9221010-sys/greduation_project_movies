dd8cac956edcb5b96a49674be4fd091c60b2e834
MohamedShawky <shawkymohamed138@gmail.com>
2026-09-07 01:11:15 +0300

    save the current work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greduation_movies_fluter/ui/profile_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: const ProfileScreen(),
     );
   }
 }
